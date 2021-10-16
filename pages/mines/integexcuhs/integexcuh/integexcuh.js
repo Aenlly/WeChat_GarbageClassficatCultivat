510438bfed6e3bc97122562ea3fdaca55aadc1d7
Aenlly <2645274834@qq.com>
2021-10-16 21:41:35 +0800

    积分兑换设计完成

--- pages/mines/integexcuhs/integexcuh/integexcuh.js
+++ pages/mines/integexcuhs/integexcuh/integexcuh.js
@@ -5,62 +5,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    presentwhere: [{
+      imageUrl: "https://placeimg.com/640/480/any",
+      text: "垃圾桶"
+    }, {
+      imageUrl: "https://placeimg.com/640/480/any",
+      text: "垃圾桶"
+    }, {
+      imageUrl: "https://placeimg.com/640/480/any",
+      text: "垃圾桶"
+    }, {
+      imageUrl: "https://placeimg.com/640/480/any",
+      text: "垃圾桶"
+    }],
+    present: [{
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }, {
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }, {
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }, {
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }, {
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }, {
+      present_id: 1,
+      name: "垃圾桶",
+      imageUrl: "",
+      integr: 300,
+      price: 36.5,
+      number: 30
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
