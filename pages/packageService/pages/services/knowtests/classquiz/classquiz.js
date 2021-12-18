22a7ca7eca61596d8d596514e269f98c8aaba746
Aenlly <2645274834@qq.com>
2021-12-18 16:56:20 +0800

    增加分类小考请求完成

--- pages/packageService/pages/services/knowtests/classquiz/classquiz.js
+++ pages/packageService/pages/services/knowtests/classquiz/classquiz.js
@@ -1,130 +1,144 @@
-// pages/packageService/pages/services/knowtests/classquiz/classquiz.js
+// pages/packageService/pages/services/knowtests/answereveryday/Answereveryday.js
+const app = getApp()
+//获得请求地址
+const API_URL = app.globalData.API_URL
+var userId = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    answerveryday: {
-      question_id: 1,
-      question_type: 1,
-      topic: [
-        {
-          topic_id: 0,
-          topic_name: "1、以下哪个是可回收垃圾？",
-          analysis: "因为塑料瓶属于可再利用",
-          option: [{
-            option_id: 1,
-            option_name: "A、贝壳",
-            iscorrect: false,
-          }, {
-            option_id: 2,
-            option_name: "B、电池",
-            iscorrect: false,
-          }, {
-            option_id: 3,
-            option_name: "C、菜叶",
-            iscorrect: false,
-          }, {
-            option_id: 4,
-            option_name: "D、塑料瓶",
-            iscorrect: true,
-          }]
-        }, {
-          topic_id: 2,
-          topic_name: "2、以下哪个是可回收垃圾？",
-          option: [{
-            option_id: 1,
-            option_name: "A、贝壳",
-            iscorrect: false,
-          }, {
-            option_id: 2,
-            option_name: "B、电池",
-            iscorrect: false,
-          }, {
-            option_id: 3,
-            option_name: "C、菜叶",
-            iscorrect: false,
-          }, {
-            option_id: 4,
-            option_name: "D、塑料瓶",
-            iscorrect: true,
-          }]
-        }
-      ]
-    },
+    type: '每日答题',
+    topics: [{
+      topic_id: 0,
+      topic_name: "1、以下哪个是可回收垃圾？",
+      analysis: "因为塑料瓶属于可再利用",
+      option: [{
+        option_id: 1,
+        option_name: "A、贝壳",
+        iscorrect: false,
+      }, {
+        option_id: 2,
+        option_name: "B、电池",
+        iscorrect: false,
+      }, {
+        option_id: 3,
+        option_name: "C、菜叶",
+        iscorrect: false,
+      }, {
+        option_id: 4,
+        option_name: "D、塑料瓶",
+        iscorrect: true,
+      }]
+    }, {
+      topic_id: 2,
+      topic_name: "2、以下哪个是可回收垃圾？",
+      option: [{
+        option_id: 1,
+        option_name: "A、贝壳",
+        iscorrect: false,
+      }, {
+        option_id: 2,
+        option_name: "B、电池",
+        iscorrect: false,
+      }, {
+        option_id: 3,
+        option_name: "C、菜叶",
+        iscorrect: false,
+      }, {
+        option_id: 4,
+        option_name: "D、塑料瓶",
+        iscorrect: true,
+      }]
+    }],
+    //正确选项的样式
     correct: {
-      id: 0,
+      id: 0, //正确选项编号
       background: "#6ac461",
     },
+    //错误选项的样式
     nocorrect: {
-      id: 0,
-      background: "#fa5151",
+      id: 0, //正确选项编号
+      background: "#fa5151", //样式
     },
-    topic_index: 0,
-    option_selet: 0,
+    topic_index: 0, //题目索引
+    option_selet: 0, //题目选项
+    //按钮信息
     btninfo: {
-      isanalysis: true,
-      isbtn: true,
+      isanalysis: true, //是否显示解析，默认不显示
+      isbtn: true, //是否显示按钮，默认不显示
       next_btn: "下一题",
-      ischeck: false,
-      first: 1,
+      ischeck: false, //选项是否可点击
+      first: 1, //第一次交卷
     },
+    randomIndex: '', //随机组卷批次号
     countdown: 10,
-    interval: "",
+    // interval: "",
   },
 
-  // 选择操作
+  /**
+   * 选项事件
+   * @param {*} e 
+   */
   optionCheck(e) {
     var _this = this
     var btninfo = _this.data.btninfo
     if (!btninfo.ischeck) {
-      var topiclength = _this.data.answerveryday.topic.length
+      var topiclength = _this.data.topics.length
       var topic_index = _this.data.topic_index
       var correct = _this.data.correct
       var nocorrect = _this.data.nocorrect
       // 选择了选项后同时清除定时器
-      clearInterval(_this.data.interval);
+      // clearInterval(_this.data.interval);
+
+      //获得选择选项的id
       var option_select = e.target.id
       btninfo.isanalysis = false
       btninfo.isbtn = false
       btninfo.ischeck = true
-      // 每一题的get请求
+
       wx.request({
-        url: '', //api地址
+        url: API_URL + '/answer-question/isCorrect',
+        method: "POST",
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
         data: {
-          topic_id: 0,
-          option_id: 0,
-          user_id: 0,
-          bequestion: 0
+          userId: userId,
+          topicId: _this.data.topics[topic_index].belongTopicId,
+          optionId: option_select,
+          randomIndex: _this.data.randomIndex
         },
         success(res) {
-          console.log(res.data)
-        }
-      })
-      // 判断是否正确
-      if (e.target.dataset.iscorrect) {
-        correct.id = option_select
-      } else {
-        var optionlist = _this.data.answerveryday.topic[topic_index].option
-        // 匹配正确项
-        for (var index in optionlist) {
-          if (optionlist[index].iscorrect) {
-            correct.id = optionlist[index].option_id
-            break
+          let data = res.data
+          if (data.code == 200) {
+            console.log(data)
+            //正确选项样式
+            correct.id = data.data
+            // 判断是否正确
+            if (option_select != data.data) {
+              //错误选项样式
+              nocorrect.id = option_select
+            }
+            // 判断下一题是否为最后一题
+            if (topic_index + 1 == topiclength) {
+              btninfo.next_btn = "交卷"
+            }
+            // 修改数据
+            _this.setData({
+              correct: correct,
+              nocorrect: nocorrect,
+              btninfo: btninfo
+            })
+          } else {
+            wx.showToast({
+              title: '服务器异常！',
+              icon:'error',
+              mask: true
+            })
           }
         }
-        nocorrect.id = option_select
-      }
-      // 判断下一题是否为最后一题
-      if (topic_index + 1 == topiclength) {
-        btninfo.next_btn = "交卷"
-      }
-      // 修改数据
-      _this.setData({
-        correct: correct,
-        nocorrect: nocorrect,
-        btninfo: btninfo
       })
     }
   },
@@ -135,12 +149,13 @@ Page({
     var btninfo = _this.data.btninfo
     // 判断按钮的文本
     if (_this.data.btninfo.next_btn == "下一题") {
-      topic_index++
-      btninfo.isbtn = true
-      btninfo.ischeck = false
-      btninfo.isanalysis = true
+      topic_index++ //题目索引增加
+      btninfo.isbtn = true //设置下一题按钮隐藏
+      btninfo.ischeck = false //设置选项按钮可单击
+      btninfo.isanalysis = true //设置解析隐藏
+
       _this.setData({
-        countdown: 10,
+        // countdown: 10,
         topic_index: topic_index,
         btninfo: btninfo,
         correct: {
@@ -152,7 +167,7 @@ Page({
           background: "#fa5151",
         },
       })
-      _this.startInterval()
+      // _this.startInterval()
     }
     // 第一次点交卷才有效果
     else if (btninfo.first == 1) {
@@ -162,25 +177,46 @@ Page({
       })
       // 交卷请求
       wx.request({
-        url: '', //api地址
+        url: API_URL + '/answer-question/submitPaper', //api地址
         data: {
-          topic_id: 0,
-          option_id: 0,
-          user_id: 0,
-          bequestion: 0
+          userId: userId,
+          randomIndex: _this.data.randomIndex
+        },
+        method: "POST",
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
         },
         success(res) {
-          wx.showToast({
-            title: "交卷成功",
-            mask: true
-          })
-          // 延迟自动返回
-          setTimeout(function (e) { wx.navigateBack() }, 1500)
-          console.log(res.data)
-        }
-        , fail: function () {
+          let data = res.data
+          if (data.code == 200) {
+            if (data.data) {
+              wx.showToast({
+                title: "交卷成功",
+                mask: true
+              })
+              // 延迟自动返回
+              setTimeout(function (e) {
+                wx.navigateBack()
+              }, 500)
+            } else {
+              wx.showToast({
+                title: "交卷失败",
+                icon:'error',
+                mask: true
+              })
+            }
+          } else {
+            wx.showToast({
+              title: "服务器异常！",
+              icon:'error',
+              mask: true
+            })
+          }
+        },
+        fail: function () {
           wx.showToast({
             title: "交卷失败",
+            icon:'error',
             mask: true
           })
         }
@@ -188,14 +224,15 @@ Page({
     }
   },
 
+  /* 去除倒计时功能
   // 启动定时器
   startInterval: function (e) {
     var _this = this
     var countdown = _this.data.countdown
-    var topiclength = _this.data.answerveryday.topic.length
+    var topiclength = _this.data.topics.length
     var topic_index = _this.data.topic_index
     var btninfo = _this.data.btninfo
-    var optionlist = _this.data.answerveryday.topic[topic_index].option
+    var optionlist = _this.data.topics[topic_index].option
     var correct = _this.data.correct
     // 设置定时器/1s执行一次，并且获得返回值用于清除
     var interval = setInterval(function () {
@@ -230,13 +267,46 @@ Page({
     _this.setData({
       interval: interval,
     })
-  },
+  },*/
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var _this = this
+    userId = app.globalData.userId
+    if (userId == null || userId == '') {
+      wx.showToast({
+        title: '请先登录！',
+        icon:'error',
+        mask: true
+      })
+      return
+    } else {
+      wx.request({
+        url: API_URL + '/answer-question/getTopics',
+        data: {
+          userId: userId,
+          randomIndex: options.index
+        },
+        success(res) {
+          let data = res.data
+          console.log(data)
+          if (data.code == 200) {
+            _this.setData({
+              topics: data.data,
+              randomIndex: options.index
+            })
+          } else {
+            wx.showToast({
+              title: '请求数据异常！',
+              icon:'error',
+              mask: true
+            })
+          }
+        }
+      })
+    }
   },
 
   /**
@@ -250,7 +320,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.startInterval()
+    // this.startInterval()
   },
 
   /**
