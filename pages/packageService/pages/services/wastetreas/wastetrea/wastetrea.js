988bfcfd313cdd8d7d6ccfebb6fa60334d9d84da
Aenlly <2645274834@qq.com>
2021-12-18 21:59:01 +0800

    增加选项卡信息请求

--- pages/packageService/pages/services/wastetreas/wastetrea/wastetrea.js
+++ pages/packageService/pages/services/wastetreas/wastetrea/wastetrea.js
@@ -1,16 +1,22 @@
 // pages/services/wastetrea/wastetrea.js
+const app = getApp()
+//获得请求地址
+const API_URL = app.globalData.API_URL
+var userId=''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+        //资源请求地址
+        API_RES_URL: getApp().globalData.API_RES_URL,
     tabs: [{
       title: "好用",
-      wastetrea: [{
+      WasteTurnTreasures: [{
         wastetrea_id: 0,
-        title: "小程序开发进阶",
-        desc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
+        text: "小程序开发进阶",
+        textDesc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
         imgUrl: "http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg",
         tag: 0,
         user_id: 1
@@ -38,7 +44,7 @@ Page({
     this.setData({
       activeTab: index
     })
-    console.log(this.data)
+    this.getListByTag(index)
   },
   // 卡片跳转事件
   onClickNav(e) {
@@ -89,9 +95,36 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getListByTag(this.data.activeTab)
     this.setData({
       search: this.search.bind(this),
       is_sear: false
+    })
+  },
+
+  /**
+   * 查询信息
+   * @param {选项卡索引} index 
+   */
+  getListByTag(index){
+    userId=app.globalData.userId
+    var _this=this
+    wx.request({
+      url: API_URL+'/waste-turn-treasure/getListByTag/'+index,
+      success(res){
+        let data=res.data
+        if(data.code==200){
+          console.log(data.data)
+          _this.setData({
+            ["tabs["+index+"].WasteTurnTreasures"]:data.data
+          })
+        }else{
+          wx.showToast({
+            title: '请求数据异常！',
+            icon:'error'
+          })
+        }
+      }
     })
   },
 
