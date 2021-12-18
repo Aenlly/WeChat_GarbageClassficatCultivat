9b5fd850c68cd6bc6872c74cb9074aedfd9e3ce7
Aenlly <2645274834@qq.com>
2021-12-18 22:17:42 +0800

    用户服务-变废为宝-搜索请求完成

--- pages/packageService/pages/services/wastetreas/wastetrea/wastetrea.js
+++ pages/packageService/pages/services/wastetreas/wastetrea/wastetrea.js
@@ -2,39 +2,39 @@
 const app = getApp()
 //获得请求地址
 const API_URL = app.globalData.API_URL
-var userId=''
+var userId = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-        //资源请求地址
-        API_RES_URL: getApp().globalData.API_RES_URL,
+    //资源请求地址
+    API_RES_URL: getApp().globalData.API_RES_URL,
     tabs: [{
-      title: "好用",
-      WasteTurnTreasures: [{
-        wastetrea_id: 0,
-        text: "小程序开发进阶",
-        textDesc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
-        imgUrl: "http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg",
-        tag: 0,
-        user_id: 1
-      }, {
-        wastetrea_id: 0,
-        title: "小程序开发进阶",
-        desc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
-        imgUrl: "http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg",
-        tag: 0,
-        user_id: 1
-      }]
-    },
-    {
-      title: "好看"
-    },
-    {
-      title: "好玩"
-    }
+        title: "好用",
+        WasteTurnTreasures: [{
+          wastetrea_id: 0,
+          text: "小程序开发进阶",
+          textDesc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
+          imgUrl: "http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg",
+          tag: 0,
+          user_id: 1
+        }, {
+          wastetrea_id: 0,
+          title: "小程序开发进阶",
+          desc: "本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。",
+          imgUrl: "http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg",
+          tag: 0,
+          user_id: 1
+        }]
+      },
+      {
+        title: "好看"
+      },
+      {
+        title: "好玩"
+      }
     ],
     activeTab: 0,
   },
@@ -44,6 +44,7 @@ Page({
     this.setData({
       activeTab: index
     })
+    //查询信息
     this.getListByTag(index)
   },
   // 卡片跳转事件
@@ -60,18 +61,40 @@ Page({
   },
   // 搜索事件
   search: function (value) {
+    console.log(value)
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         this.setData({
           is_sear: true
         })
-        resolve([{ text: '标题', wiki_id: 1, index: 3 }])
+        wx.request({
+          url: API_URL + '/waste-turn-treasure/getListSearchByTitle/' + value,
+          success(res) {
+            let data = res.data
+            if (data.code == 200) {
+              resolve(data.data)
+            } else {
+              wx.showToast({
+                title: '搜索请求异常！',
+                icon: 'error'
+              })
+            }
+          },
+          fail() {
+            wx.showToast({
+              title: '搜索失败！',
+              icon: 'error'
+            })
+          }
+        })
       }, 500)
     })
   },
   // 选择搜索结果事件
   selectResult: function (e) {
-    console.log('select result', e.detail)
+    wx.navigateTo({
+      url: '/pages/packageService/pages/services/wastetreas/wastetreainfo/wastetreainfo?id=' + e.detail.item.id,
+    })
   },
   // 我的事件跳转
   clickMy: function () {
@@ -106,22 +129,22 @@ Page({
    * 查询信息
    * @param {选项卡索引} index 
    */
-  getListByTag(index){
-    userId=app.globalData.userId
-    var _this=this
+  getListByTag(index) {
+    userId = app.globalData.userId
+    var _this = this
     wx.request({
-      url: API_URL+'/waste-turn-treasure/getListByTag/'+index,
-      success(res){
-        let data=res.data
-        if(data.code==200){
+      url: API_URL + '/waste-turn-treasure/getListByTag/' + index,
+      success(res) {
+        let data = res.data
+        if (data.code == 200) {
           console.log(data.data)
           _this.setData({
-            ["tabs["+index+"].WasteTurnTreasures"]:data.data
+            ["tabs[" + index + "].WasteTurnTreasures"]: data.data
           })
-        }else{
+        } else {
           wx.showToast({
             title: '请求数据异常！',
-            icon:'error'
+            icon: 'error'
           })
         }
       }
