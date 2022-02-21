093c1d8f73a0cf5f8a7ff506b0a4d43153d08708
Aenlly <2645274834@qq.com>
2022-02-21 23:21:15 +0800

    资源请求修改为ssl

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ App({
     // API_URL: "https://localhost:8002", //本地接口请求地址
     API_URL:"https://www.aenlly.top:8002",//远程接口请求地址
     // API_RES_URL: "http://localhost:8004", //本地资源请求地址
-    API_RES_URL:"http://www.aenlly.top:8004",//远程资源请求地址
+    API_RES_URL:"https://www.aenlly.top:8004",//远程资源请求地址
     userInfo: null,
     userId: null
   },
