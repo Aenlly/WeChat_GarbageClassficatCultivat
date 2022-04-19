79a23c18d2a7faf7a09b960e01181b1dd7953b8c
aenlly <2645274834@qq.com>
2022-04-20 00:02:07 +0800

    修改为远程请求地址

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
 App({
   globalData: {
-    API_URL: "https://localhost:8002", //本地接口请求地址
-    // API_URL: "https://www.aenlly.top:8002", //远程接口请求地址
+    // API_URL: "https://localhost:8002", //本地接口请求地址
+    API_URL: "https://www.aenlly.top:8002", //远程接口请求地址
     // API_RES_URL: "https://localhost:8004", //本地资源请求地址
     API_RES_URL: "https://www.aenlly.top:8004", //远程资源请求地址
     userInfo: null,
